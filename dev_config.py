8f549a6b1c08f825505fb24cc1d8ecbaecc4b444
Shlomo-T <shlontz@gmail.com>
2017-10-04 10:28:26 +0300

    Moved CACHE_PATH to configuration file and changed it from constant to configurable env var

--- dev_config.py
+++ dev_config.py
@@ -5,3 +5,5 @@ import sys
 CENSYS_API_KEY = os.environ.get('CENSYS_API_KEY', None)
 CENSYS_API_SECRET = os.environ.get('CENSYS_API_SECRET', None)
 
+CACHE_PATH = os.environ.get('REP_CACHE_PATH', '~/cache')
+
