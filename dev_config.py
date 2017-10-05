bdc7485450c36d90601b530c89ab0e02fc0999e8
Shlomo Tadela <shlomotadela@Eranits-MacBook-Pro.local>
2017-10-05 17:07:16 +0300

    Removed credentials env vars from config file

--- dev_config.py
+++ dev_config.py
@@ -1,9 +1,5 @@
 import os
 import sys
 
-
-CENSYS_API_KEY = os.environ.get('CENSYS_API_KEY', None)
-CENSYS_API_SECRET = os.environ.get('CENSYS_API_SECRET', None)
-
-CACHE_PATH = os.environ.get('REP_CACHE_PATH', '~/cache')
+CACHE_PATH = os.environ.get('REP_CACHE_PATH', '/cache')
 
